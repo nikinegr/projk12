214c6522fc0c361952a413516588cd3d912a04f7
Пользователь <nik.kozlukov@gmail.com>
2023-12-29 19:51:02 +0200

    124o12

--- django_test3/django_test3/app_10_22/urls.py
+++ django_test3/django_test3/app_10_22/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('project_create/', views. Create_project.as_view(), name='pr'),
     path('registration/', views. Registration.as_view()),
     path('project/<int:id>/', views.project),
-    path('edit project/<int:id>/', views.edit_project),
-    path('test/', views. TestPage.as_view())
+    path('edit project/<int:id>/', views.edit_project)
+#path('test/', views. TestPage.as_view())
 ]
